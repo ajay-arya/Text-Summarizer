b91fe06e280f8d9b10158dcc7fb74c68fdccc00c
ajay-arya <ajayarya512@gmail.com>
2019-04-06 10:29:04 +0530

    working till now

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, flash
 from werkzeug.utils import secure_filename
+import os
+from optimize import store
+
+UPLOAD_FOLDER = 'uploadedFiles'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'doc', 'docx'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowedFile(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/')
@@ -11,13 +22,23 @@ def Index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    #    if request.method == 'POST':
-    #       file = request.files['inputFile']
-    #       f.save(secure_filename(f.filename))
-    #       return render_template('sucess.html')
+    # file = request.files['inputFile']
+
+    # return file.filename
     file = request.files['inputFile']
-    
-    return file.filename
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    if file and allowedFile(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return file.filename
+    else:
+        return render_template('fail.html')
+
+@app.route('/getSummary')
+def summarizedFile():
+    return store()
 
 
 if __name__ == "__main__":
